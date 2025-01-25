245f37ea677a1d33065a47aa3efd21f7d00b8945
MarcosFGimenes <marcos.gimenes010@gmail.com>
2025-01-25 00:29:08 -0300

    Alterando metodo do MONGODB

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -8,18 +9,15 @@ const app = express();
 
 // Configurações de CORS
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://camylash.vercel.app', 'https://camylash-production.up.railway.app'], // URLs permitidas
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://camylash.vercel.app', 'http://mongodb.camilygimenes.kinghost.net', 'http://camilydesigner.yex1d.mongodb.net'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 // Middleware para pré-verificação (OPTIONS)
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
     return res.status(200).json({});
@@ -30,14 +28,12 @@ app.use((req, res, next) => {
 // Configurações de body-parser
 app.use(bodyParser.json());
 
-// Conexão com o MongoDB
-mongoose
-  .connect(
-    'mongodb://mongo:HqOhLmRhOItvfzmuHOqhEUnqnxnsiQVR@monorail.proxy.rlwy.net:45032',
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log('Conectado ao MongoDB com sucesso!'))
-  .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
+// Conexão com o MongoDB via Mongoose
+const uri = "mongodb+srv://marcosgimenesdev:bsjr8wErzR3QPgX5@camilydesigner.yex1d.mongodb.net/camilydesigner?retryWrites=true&w=majority";
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Conectado ao MongoDB com sucesso!"))
+  .catch((error) => console.error("Erro ao conectar ao MongoDB:", error));
 
 // Rotas de agendamento
 app.use('/api', agendamentoRoutes);
